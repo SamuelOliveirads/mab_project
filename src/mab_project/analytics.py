609ab3ed00cf4a7627869c851eb48877db10682c
samuel <samueloliveira32df@gmail.com>
2023-06-04 15:04:58 -0300

    mab-project (refactor): refactor test analysis

--- src/mab_project/analytics.py
+++ src/mab_project/analytics.py
@@ -1,13 +1,20 @@
+from pathlib import Path
+from typing import Tuple
+
 import numpy as np
 import pandas as pd
-from scipy import stats
 from matplotlib import pyplot as plt
 from matplotlib.animation import FuncAnimation
+from scipy import stats
+
+NUM_MONTE_CARLO_SIMS = 1000
+SCALE_FACTOR = 1.25
+DATA_PATH = Path("../../data/02_intermediate/update_experiment.csv")
 
 
-def bayesian_inference(data):
-    N_mc = 1000
-
+def calculate_stats_and_samples(
+    data: pd.DataFrame, day: int, group: str
+) -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
     proba_b_better_a = []
     expected_loss_a =[]
     expected_loss_b =[]
@@ -22,40 +29,74 @@ def bayesian_inference(data):
         # Draw samples from normal distributions for both groups
         x_a = np.random.normal(loc=u_a, scale=1.25*np.sqrt(var_a), size=N_mc)
         x_b = np.random.normal(loc=u_b, scale=1.25*np.sqrt(var_b), size=N_mc)
+    beta_mean, beta_variance = stats.beta.stats(
+        a=1 + data.loc[day, f"acc_clicks_{group}"],
+        b=1
+        + data.loc[day, f"acc_visits_{group}"]
+        - data.loc[day, f"acc_clicks_{group}"],
+        moments="mv",
+    )
 
-        # Calculate the beta pdf for both groups
-        fa = stats.beta.pdf(x_a,
-                            a = 1 + data.loc[day, 'acc_clicks_a'],
-                            b = 1 + (data.loc[day, 'acc_visits_a'] - data.loc[day, 'acc_clicks_a'])
-                                )
+    normal_samples = np.random.normal(
+        loc=beta_mean,
+        scale=SCALE_FACTOR * np.sqrt(beta_variance),
+        size=NUM_MONTE_CARLO_SIMS,
+    )
 
-        fb = stats.beta.pdf(x_b,
-                            a = 1 + data.loc[day, 'acc_clicks_b'],
-                            b = 1 + (data.loc[day, 'acc_visits_b'] - data.loc[day, 'acc_clicks_b'])
-                                )
+    beta_pdf = stats.beta.pdf(
+        normal_samples,
+        a=1 + data.loc[day, f"acc_clicks_{group}"],
+        b=1
+        + (data.loc[day, f"acc_visits_{group}"] - data.loc[day, f"acc_clicks_{group}"]),
+    )
 
-        # Calculate the normal pdf for both groups
-        ga = stats.norm.pdf(x_a,
-                            loc=u_a,
-                            scale=1.25*np.sqrt(var_a))
+    normal_pdf = stats.norm.pdf(
+        normal_samples, loc=beta_mean, scale=SCALE_FACTOR * np.sqrt(beta_variance)
+    )
 
+    return normal_samples, beta_pdf, normal_pdf
+
+
+def bayesian_inference(data: pd.DataFrame) -> Tuple[list, list, list]:
         gb = stats.norm.pdf(x_b,
                             loc=u_b,
                             scale=1.25*np.sqrt(var_b))
+    prob_b_better_than_a_list = []
+    expected_loss_a_list = []
+    expected_loss_b_list = []
 
-        # Calculate y as the ratio of the beta and normal pdfs
-        y = (fa*fb) / (ga*gb)
+    for day in range(len(data)):
+        samples_a, beta_pdf_a, normal_pdf_a = calculate_stats_and_samples(
+            data, day, "a"
+        )
+        samples_b, beta_pdf_b, normal_pdf_b = calculate_stats_and_samples(
+            data, day, "b"
+        )
 
-        # Filter samples where group B is better than group A
-        yb = y[x_b >= x_a]
+        # Calculate ratio_beta_normal_pdfs as the ratio of the beta and normal pdfs
+        ratio_beta_normal_pdfs = (beta_pdf_a * beta_pdf_b) / (
+            normal_pdf_a * normal_pdf_b
+        )
 
         # Calculate probability for B is better than A
-        p = (1 / N_mc) * np.sum(yb)
+        y_b_greater_than_a = ratio_beta_normal_pdfs[samples_b >= samples_a]
+        prob_b_better_than_a = (1 / NUM_MONTE_CARLO_SIMS) * np.sum(y_b_greater_than_a)
+        prob_b_better_than_a_list.append(prob_b_better_than_a)
 
-        # Expected losses for B is better than A
-        expected_loss_A = (1/N_mc) * np.sum(((x_b - x_a)*y)[x_b >= x_a])
-        expected_loss_B = (1/N_mc) * np.sum(((x_a - x_b)*y)[x_a >= x_b])
+        # Calculate expected losses for both groups
+        expected_loss_for_a = (1 / NUM_MONTE_CARLO_SIMS) * np.sum(
+            ((samples_b - samples_a) * ratio_beta_normal_pdfs)[samples_b >= samples_a]
+        )
+        expected_loss_for_b = (1 / NUM_MONTE_CARLO_SIMS) * np.sum(
+            ((samples_a - samples_b) * ratio_beta_normal_pdfs)[samples_a >= samples_b]
+        )
+        expected_loss_a_list.append(expected_loss_for_a)
+        expected_loss_b_list.append(expected_loss_for_b)
 
+    return prob_b_better_than_a_list, expected_loss_a_list, expected_loss_b_list
+
+
+def load_and_process_data(data_path: Path) -> pd.DataFrame:
         proba_b_better_a.append(p)
         expected_loss_a.append(expected_loss_A)
         expected_loss_b.append(expected_loss_B)
@@ -65,15 +106,32 @@ def bayesian_inference(data):
 
 def animate_plot(data_experiment):
     data = pd.read_csv("../../data/02_intermediate/update_experiment.csv")
+    data = pd.read_csv(data_path)
 
     data["click"] = data["click"].astype(int)
     data["visit"] = data["visit"].astype(int)
 
     # pivot table
-    data = data.reset_index().rename(columns={'index': 'day'})
-    data = data.pivot(index='day', columns='group', values=['click', 'visit']).fillna(0)
-    data.columns = ['click_control', 'click_treatment', 'visit_control', 'visit_treatment']
+    data = data.reset_index().rename(columns={"index": "day"})
+    data = data.pivot(index="day", columns="group", values=["click", "visit"]).fillna(0)
+    data.columns = [
+        "click_control",
+        "click_treatment",
+        "visit_control",
+        "visit_treatment",
+    ]
     data = data.reset_index(drop=True)
+
+    data["acc_visits_a"] = data["visit_control"].cumsum()
+    data["acc_clicks_a"] = data["click_control"].cumsum()
+
+    data["acc_visits_b"] = data["visit_treatment"].cumsum()
+    data["acc_clicks_b"] = data["click_treatment"].cumsum()
+
+    return data
+
+
+def animate_plot(i: int, data: pd.DataFrame):
     
     data['acc_visits_a'] = data['visit_control'].cumsum()
     data['acc_clicks_a'] = data['click_control'].cumsum()
@@ -82,19 +140,23 @@ def animate_plot(data_experiment):
     data['acc_clicks_b'] = data['click_treatment'].cumsum()
 
     # inferencet bayesian
-    p, expected_loss_a, expected_loss_b = bayesian_inference(data)
+    prob_b_better_than_a, expected_loss_a, expected_loss_b = bayesian_inference(data)
 
-    x1 = np.arange(len(p))
+    days = np.arange(len(prob_b_better_than_a))
 
     plt.cla()
-    plt.plot(x1, p, label="Probability B better A")
-    plt.plot(x1, expected_loss_a, label="Risk Choosing A")
-    plt.plot(x1, expected_loss_b, label="Risk Choosing B")
+    plt.plot(days, prob_b_better_than_a, label="Probability B better A")
+    plt.plot(days, expected_loss_a, label="Risk Choosing A")
+    plt.plot(days, expected_loss_b, label="Risk Choosing B")
     plt.legend(loc="upper left")
     plt.tight_layout()
 
 
-ani = FuncAnimation(plt.gcf(), animate_plot, interval=1000)
+def main():
+    data = load_and_process_data(DATA_PATH)
+    ani = FuncAnimation(plt.gcf(), animate_plot, fargs=(data,), interval=1000)
+    plt.show()
 
-plt.tight_layout()
-plt.show()
+
+if __name__ == "__main__":
+    main()
